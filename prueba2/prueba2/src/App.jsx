691bed0615df55d7c82afc3835f22f2d6515c02f
Juan Manuel <justin4949ch@gmail.com>
2023-02-24 17:37:40 -0500

    tarea de tweet

--- prueba2/prueba2/src/App.jsx
+++ prueba2/prueba2/src/App.jsx
@@ -1,19 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect,useContext } from 'react'
 
 export const EjemploEvento = () => {
 
-  const [value, setValue] = useState('Edwin');
-  const [greatting, setGreatting] = useState('Bienvenido');
+
+  const [value, setValue] = useState('');
+  const [greatting, setGreatting] = useState('aqui veras tu tweet');
 
   const handleEvent = () => {
-    setGreatting('Hola '+value)
+    setGreatting(value )
   }
 
   return (
-    <div className='container-events'>
+    <>
+      <h1>Generador de tweets</h1>
+      <div>
+      <h2>
+        publica tu tweet
+      </h2>
+      <hr />
+      <div><textarea onChange={ (event)=> setValue(event.target.value)} id='txt-into' className='' type="text"></textarea></div>
+      <button onClick={handleEvent} className='btn-greatting'>publicar</button>
+      <button onClick={handleEvent} className='btn-greatting'>archivar</button>
+      <button onClick={handleEvent} className='btn-greatting'>mostrar archivos</button>
+      </div>
       <h3>{greatting}</h3>
-      <input onChange={ (event)=> setValue(event.target.value)} id='txt-into' className='' type="text" />
-      <button onClick={handleEvent} className='btn-greatting'>Saludar</button>
-    </div>
+    </>
   )
 }
