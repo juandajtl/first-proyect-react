a0f5bfd5682e7e2bb767bbf78eb8576d5613b6b1
Juan Manuel <justin4949ch@gmail.com>
2023-02-28 15:51:24 -0500

    se intenti agregar el contador de letreas

--- prueba2/prueba2/src/App.jsx
+++ prueba2/prueba2/src/App.jsx
@@ -5,20 +5,23 @@ export const EjemploEvento = () => {
 
   const [value, setValue] = useState('');
   const [greatting, setGreatting] = useState('aqui veras tu tweet');
-
-  const handleEvent = () => {
-    setGreatting(value )
-  }
+  const [wordCount, setWordCount] = useState(255);
+  // const { text } = useContext(TextContext);
+  const handleEvent = () => {setGreatting(value )}
+function countWords(text) {
+  return text.trim().split(/\s+/).length;
+}
 
   return (
     <>
       <h1>Generador de tweets</h1>
       <div>
-      <h2>
-        publica tu tweet
-      </h2>
+      <h2>publica tus tweets</h2>
       <hr />
-      <div><textarea onChange={ (event)=> setValue(event.target.value)} id='txt-into' className='' type="text"></textarea></div>
+      <div>
+      <textarea  onChange={ (event)=> setValue(event.target.value)} id='txt-into' className='Txt-area' type="text"></textarea>
+      </div>
+      <p >te quedan {wordCount} </p>
       <button onClick={handleEvent} className='btn-greatting'>publicar</button>
       <button onClick={handleEvent} className='btn-greatting'>archivar</button>
       <button onClick={handleEvent} className='btn-greatting'>mostrar archivos</button>
